c6035e8dcf7a06da99aaf58150eea81c2c012bf7
Nicolas Lang <git@nicolas-lang.de>
2022-01-12 21:18:57 +0100

    Removed Lab Changes, as they are now part of the K2/SE integration

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,6 +1,5 @@
 require("__LarkenxK2SETweaks__/data/K2SE_logistic_pipes_data-final-fixes")
 require("__LarkenxK2SETweaks__/data/SE_buildings_in-space_data-final-fixes")
-require("__LarkenxK2SETweaks__/data/K2SE_buildings_lab_data-final-fixes")
 require("__LarkenxK2SETweaks__/data/SE_buildings_on-spaceship_data-final-fixes")
 require("__LarkenxK2SETweaks__/data/K2SE_matterconversion_cubes_data-final-fixes")
 require("__LarkenxK2SETweaks__/data/K2SE_matterconversion_change-rates_data-final-fixes")
