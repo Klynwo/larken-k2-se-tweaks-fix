a3586945011edc48857884c16e745aecde1bd68f
Nicolas Lang <git@nicolas-lang.de>
2022-01-12 21:39:03 +0100

    Removed unnecessary code duplicates

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,4 +1,3 @@
-require("__LarkenxK2SETweaks__/data/SE_buildings_in-space_data-final-fixes")
 require("__LarkenxK2SETweaks__/data/SE_buildings_on-spaceship_data-final-fixes")
 require("__LarkenxK2SETweaks__/data/K2SE_matterconversion_cubes_data-final-fixes")
 require("__LarkenxK2SETweaks__/data/K2SE_matterconversion_change-rates_data-final-fixes")
